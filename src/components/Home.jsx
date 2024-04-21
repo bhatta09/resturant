476e31c484b51bfe9b04b21576cf1760f095adc0
bhatta09 <sandesh@academiacollege.edu.np>
2024-04-21 12:10:45 +0545

    again

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className=" min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/img/hero.jpg')] bg-cover bg-no-repeat ">
       <div className=" w-full lg:w-2/3 space-y-5">
         <h1 className=" text-backgroundColor font-semibold text-6xl">
-        A journey of <span className="text-brightColor">taste</span>, where your food discovers the extraordinary.
+        "Food of <span className="text-brightColor">taste</span>, presented by the kitchen."
         </h1>
         <p className=" text-backgroundColor">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis et qui,
